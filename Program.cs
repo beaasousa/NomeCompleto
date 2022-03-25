991507b6659a703bee7454772f48e24ba892d64a
Ana Beatriz <anacostasousa08@gmail.com>
2022-03-25 20:31:39 -0300

    finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome, sobrenomeEmMaiusculas;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!; 
+
+Console.Write("Digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiusculas = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de Catálogo: {sobrenomeEmMaiusculas}, {nome}");
